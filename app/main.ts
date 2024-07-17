8590e86c222b9b1f4a44e4c736bcef3c352e79eb
Sneha Kumari <siddharth8271skg2@gmail.com>
2024-07-17 16:53:14 +0530

    extraction of header endpoint

--- app/main.ts
+++ app/main.ts
@@ -1,17 +1,17 @@
-
 import * as net from 'net';
+
 const server = net.createServer((socket) => {
    socket.on('data' , (data) => {
     const req = data.toString();
     const url =  req.split(' ')[1];
     const res =  url === '/' ? 'HTTP/1.1 200 OK\r\n\r\n' : 'HTTP/1.1 404 Not Found\r\n\r\n';
     const str = url.split('/')[2];
-    const lines = url.split('\r\n');
+    const lines = req.split('\r\n'); // Corrected to split the entire request by \r\n
 
    let userAgent = ''
 
    for(let line of lines){
-    if(line.startsWith('User-Agent: ')){
+    if(line.startsWith('User-Agent:')){
         userAgent = line.split(':')[1];
         break;
     }
@@ -21,7 +21,7 @@ const server = net.createServer((socket) => {
     if (url === '/') {
         socket.write('HTTP/1.1 200 OK\r\n\r\n');
     } else if (url === '/user-agent') {
-        socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n${userAgent}`);
+        socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`);
     } 
     else if (url === `/echo/${str}`) {
       
